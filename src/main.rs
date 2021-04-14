476a60b636c24035a2c8019c8323a305ec826cd4
Nick <speedyleion@users.noreply.github.com>
2021-04-13 19:38:55 -0700

    re-working directory logic

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,23 @@ use jwalk::WalkDir;
 use std::env;
 use std::time::Instant;
 
+use directory_walking::ntqeurydirectoryfile::dir_walk;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let start = Instant::now();
     let count = run_jwalk(&args[1]);
     let end = Instant::now();
-    println!("The time {:?}, and the files {:?}", end.duration_since(start), count);
+    println!("The jwalk time {:?}, and the files {:?}", end.duration_since(start), count);
+
+    let start = Instant::now();
+    let count = dir_walk(&args[1]);
+    let end = Instant::now();
+    println!("The ntquerydirectoryfile time {:?}, and the files {:?}", end.duration_since(start), count);
 }
 
-fn run_jwalk(directory: &String) -> u32 {
+fn run_jwalk(directory: &str) -> u32 {
     let mut count = 0;
     for _ in WalkDir::new(directory).sort(true) {
         count += 1;
